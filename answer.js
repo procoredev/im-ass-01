5b563b08bfb47525151f2afc8c20eaa4f6ce3b44
IMRAN HOSSEN <ftrlabib@gmail.com>
2026-09-07 22:37:07 +0600

    second input

--- answer.js
+++ answer.js
@@ -30,3 +30,22 @@ function getDayType (day){
     }
 }
 
+// Question no 03---
+function validateUsername (username){
+    if (username.length < 4) {
+        return "Too Short";
+    }
+
+    if (username.length ("")){
+       return "No Space Allowed"; 
+    }
+
+    if (username.toLowerCase().includes("admin")){
+        return "Reserved Word";
+    }
+
+    return "Available";
+
+}
+
+// Question no 03---
